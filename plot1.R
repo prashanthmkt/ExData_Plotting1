0150d197b20d8d365701b98a648b7ff26f741d7f
prashanthmkt <prashanthkumar.mkt@gmail.com>
2014-10-19 13:31:24 +0530

    created plot1

--- plot1.R
+++ plot1.R
@@ -1,1 +1,14 @@
+## getting fulldata
+
+data<- read.table(("household_power_consumption.txt"),sep=";",header=TRUE)
+
+## To Make Histogram
+
+hist (as.numeric(data$Global_active_power), xlab="Global Active Power(kilowatts)",ylab="Frequency", main="Global Active Power", col="red")
+
+## To copy png file
+
+dev.copy(png,file="plot1.png", height=480,width=480)
+
+dev.off()
 
